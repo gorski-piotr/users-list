a31e9aff89e20326194f9b76398dc5f01e2455cf
Piotr Gorski <gorskipiotr5@gmail.com>
2021-03-29 09:25:20 +0100

    small changes

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -4,7 +4,7 @@ function AddUser() {
     return (
         <div className="d-flex justify-content-center">
             <input className="m-2" type="text" name="name" id="name" placeholder="User name"/>
-            <buttton className="btn btn-primary m-2">Add name</buttton>
+            <button className="btn btn-primary m-2">Add name</button>
         </div>
     )
 }
