99b8f11436ee9f69a0807f91815ff3aa317fd98d
Piotr Gorski <gorskipiotr5@gmail.com>
2021-05-20 17:54:08 +0100

    small test change

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function Title() {
-    return (
-        <div>
-            <h1>User's list</h1>
-            <p>Created with ReactJS and Bootstrap</p>
-        </div>
-    )
+  return (
+    <div>
+      <h1>User's list</h1>
+      <p>Created with ReactJS and Bootstrap.</p>
+    </div>
+  );
 }
 
-export default Title
+export default Title;
